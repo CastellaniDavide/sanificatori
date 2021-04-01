be1010be17b5fdf00cc388f201828b31369ebef0
castellanidavide <davidecastellani@castellanidavide.it>
2021-04-01 08:32:12 +0200

    2021-04-01

--- lib/Searching.dart
+++ lib/Searching.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Searching extends StatefulWidget {
@@ -12,6 +14,48 @@ class _SearchingState extends State<Searching> {
       appBar: AppBar(
         title: Text("Scan for Bluetooth devices"),
       ),
+      body: ListView.builder(
+          padding: EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            return _buildRow("Device #" + i.toString(), _getRandomAddress(), i);
+          }
+      ),
     );
+  }
+
+  Widget _buildRow(String name, String address, int i) {
+    return ListTile(
+      title: Text(name),
+      subtitle: Text(address),
+      trailing: (
+          RaisedButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(5.0))),
+            color: Colors.grey,
+            child: Text("Connect"),
+            onPressed: (){
+              _onPressed(i);
+            },
+          )
+      ),
+      onTap: (){
+        _onPressed(i);
+      },
+    );
+  }
+
+  void _onPressed(i){
+    print("Pressed: " + i.toString());
+    //setState(() {});
+  }
+
+  String _getRandomAddress()
+  {
+    Random random = new Random();
+    String string = "";
+    for (int i = 0; i < 6; ++i)
+      string += random.nextInt(256).toRadixString(16).padLeft(2, '0').toUpperCase() + (i != 5 ? ":" : "");
+
+    return string;
   }
 }
